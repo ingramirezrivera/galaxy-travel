81b1a313d230bce3e663860b9d85960b4aea6c9d
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-10-10 13:02:30 -0500

    Navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./NavbarStyles.css";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(!click);
+  
+  
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>GLX TRVL</h1>
+      </Link>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'} onClick={closeMenu}>
+        <li className="nav-item">
+          <Link to="home">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="pricing">Pricing</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="training">Training</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburguer" onClick={handleClick}>
+      {click ? (<FaTimes style={{ color: "#ffff" }} size={30} />) : (<FaBars style={{ color: "#ffff" }} size={30} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
